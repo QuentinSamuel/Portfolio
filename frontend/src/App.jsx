dd14acd8cabd0dc6a4f90dccd1c3e720fb1ce994
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-07-20 17:44:15 +0200

    make routes to admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ProjetList from "./components/Project/ProjectList";
 import "./App.css";
 
 function App() {
-  return <ProjetList />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/projects" element={<ProjetList />} />
+        <Route path="/admin" element={<ProjetList admin />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
