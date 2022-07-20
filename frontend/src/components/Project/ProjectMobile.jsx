4f9320b3fa2ab41a103f5a7eb71bfc9477a5ac44
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-07-20 17:45:29 +0200

    make a render for admin in mobile

--- frontend/src/components/Project/ProjectMobile.jsx
+++ frontend/src/components/Project/ProjectMobile.jsx
@@ -1,24 +1,41 @@
 import { ProjectType, defaultProjectType } from "@prop-types/ProjectType";
 import Modal from "@components/Modal/Modal";
+import UpdateModal from "./UpdateModal";
 
-export default function ProjectMobile({ project }) {
+export default function ProjectMobile({ project, admin }) {
   return (
-    <Modal>
-      <Modal.Trigger>
-        <figure className="border">
-          <img src="https://picsum.photos/250/250" alt="project" />
-          <figcaption className="text-center">{project.name}</figcaption>
-        </figure>
-      </Modal.Trigger>
-      <Modal.Window>
-        <p>{project.description}</p>
-        <div>
-          {project.date_start} - {project.date_end}
-        </div>
-        <a href={project.github}>Github</a>
-        <a href={project.demo}>Demo</a>
-      </Modal.Window>
-    </Modal>
+    <div>
+      {admin ? (
+        <Modal>
+          <Modal.Trigger>
+            <figure className="border">
+              <img src="https://picsum.photos/250/250" alt="project" />
+              <figcaption className="text-center">{project.name}</figcaption>
+            </figure>
+          </Modal.Trigger>
+          <Modal.Window>
+            <UpdateModal project={project} />
+          </Modal.Window>
+        </Modal>
+      ) : (
+        <Modal>
+          <Modal.Trigger>
+            <figure className="border">
+              <img src="https://picsum.photos/250/250" alt="project" />
+              <figcaption className="text-center">{project.name}</figcaption>
+            </figure>
+          </Modal.Trigger>
+          <Modal.Window>
+            <p>{project.description}</p>
+            <div>
+              {project.date_start} - {project.date_end}
+            </div>
+            <a href={project.github}>Github</a>
+            <a href={project.demo}>Demo</a>
+          </Modal.Window>
+        </Modal>
+      )}
+    </div>
   );
 }
 
