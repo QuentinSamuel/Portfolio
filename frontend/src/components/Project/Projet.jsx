88403535dcb627af38e069135f94e4d78d2d820d
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-07-20 12:20:02 +0200

    clean code and use modal

--- frontend/src/components/Project/Projet.jsx
+++ frontend/src/components/Project/Projet.jsx
@@ -1,21 +1,24 @@
 import { ProjetType, defaultProjetType } from "@prop-types/ProjetType";
+import Modal from "@components/Modal/Modal";
 
 export default function Projet({ projet }) {
   return (
-    <>
-      <figure className="border">
-        <img src="https://picsum.photos/250/250" alt="projet" />
-        <figcaption className="text-center">{projet.name}</figcaption>
-      </figure>
-      <div className="projet-description">{projet.description}</div>
-      <div className="projet-date">
-        {projet.date_start} - {projet.date_end}
-      </div>
-      <div className="projet-links">
+    <Modal>
+      <Modal.Trigger>
+        <figure className="border">
+          <img src="https://picsum.photos/250/250" alt="projet" />
+          <figcaption className="text-center">{projet.name}</figcaption>
+        </figure>
+      </Modal.Trigger>
+      <Modal.Window>
+        <p>{projet.description}</p>
+        <div>
+          {projet.date_start} - {projet.date_end}
+        </div>
         <a href={projet.github}>Github</a>
         <a href={projet.demo}>Demo</a>
-      </div>
-    </>
+      </Modal.Window>
+    </Modal>
   );
 }
 
